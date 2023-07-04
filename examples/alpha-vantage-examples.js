d7b896c2ca62a33ed5c157d2ba91b8e9a5409baf
Developers Digest <124798203+developersdigest@users.noreply.github.com>
2023-07-03 20:04:56 -0400

    Update alpha-vantage-examples.js

--- examples/alpha-vantage-examples.js
+++ examples/alpha-vantage-examples.js
@@ -6,4 +6,5 @@ const res1 = await functionChain.call("What is Apple's market capitalization");
 const res2 = await functionChain.call("What is Microsoft's PE Ratio");
 const res3 = await functionChain.call("What is Amazon's Revenue (TTM)");
 const res4 = await functionChain.call("What is Alphabet's EBITDA");
+
 console.log(res1, res2, res3, res4);
