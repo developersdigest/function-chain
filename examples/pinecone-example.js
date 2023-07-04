336f5854749a39609ddfd5b7fb6e1efac3c29dd9
Developers Digest <124798203+developersdigest@users.noreply.github.com>
2023-07-03 23:09:16 -0400

    Update pinecone-example.js

--- examples/pinecone-example.js
+++ examples/pinecone-example.js
@@ -13,8 +13,3 @@ const update = await PineconeChain.call(
   "Add this info my pinecone index called bookshelf. With the following: This is an example of where you can fill in the text you want to be processed. The source for this is 'example.com'"
 );
 console.log(update);
-
-const query = await PineconeChain.call(
-  "Query my pinecone index called bookshelf. With the following: This is an example of where you can fill in the text you want to be processed. The source for this is 'example.com'"
-);
-console.log(query);
