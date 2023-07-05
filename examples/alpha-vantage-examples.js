f3266056425fc232fc913edf7096525020f7bdd2
Developers Digest <124798203+developersdigest@users.noreply.github.com>
2023-07-05 07:51:12 -0400

    Update alpha-vantage-examples.js

--- examples/alpha-vantage-examples.js
+++ examples/alpha-vantage-examples.js
@@ -1,11 +1,10 @@
 import { FunctionChain } from "ai-function-chain";
 
-const functionChain = new FunctionChain({ functionArray: ["getAlphaVantageCompanyOverview"] });
+const functionChain = new FunctionChain({functionArray: ["getAlphaVantageCompanyOverview"]});
 
 const res1 = await functionChain.call("What is Apple's market capitalization");
 const res2 = await functionChain.call("What is Microsoft's PE Ratio");
 const res3 = await functionChain.call("What is Amazon's Revenue (TTM)");
 const res4 = await functionChain.call("What is Alphabet's EBITDA");
 
-console.log(res1, res2, res3, res4);
-
+console.log(`1. ${res1} \n2. ${res2} \n3. ${res3} \n4. ${res4}`);
