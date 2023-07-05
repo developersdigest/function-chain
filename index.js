165aed05b85bb118dd10177cfd82fd657e668eb1
Developers Digest <124798203+developersdigest@users.noreply.github.com>
2023-07-04 22:27:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 import { FunctionChain } from "ai-function-chain";
 
-const initOptions = { functionsDirectory: "./openAIFunctions"};
-const functionChain = new FunctionChain(initOptions);
+const functionChain = new FunctionChain();
 
 const res1 = await functionChain.call("Get me the latest price of Bitcoin");
 const res2 = await functionChain.call("Open the calculator on my computer");
-const res3 = await functionChain.call("Get me the latest price of Ethereum", {
-  functionArray: ["latestPrices"] // Optionally specify which functions to use
-});
+const res3 = await functionChain.call("Get me the latest price of Ethereum");
 
 console.log(res1, res2, res3);
