e12e7f061ab940ec2df345fd578bab1c053bb78e
Developers Digest <softwaredevelopmentvideos2023@gmail.com.com>
2023-07-09 22:56:47 -0400

    add all examples

--- examples/all-examples.js
+++ examples/all-examples.js
@@ -1,6 +1,5 @@
 import {
   FunctionChain,
-  wikipedia,
   fetchCryptoPrice,
   getAlphaVantageCompanyOverview,
   getAlphaVantageIntraday,
@@ -11,91 +10,41 @@ import {
   takeScreenshot,
   getVisualCrossingWeatherForecast,
   huggingFaceImageClassification,
+  wikipedia,
 } from "ai-function-chain";
 
-async function runAllExamples() {
-  const functionChain = new FunctionChain({
-    functions: [
-      wikipedia,
-      fetchCryptoPrice,
-      getAlphaVantageCompanyOverview,
-      getAlphaVantageIntraday,
-      askPinecone,
-      createPinecone,
-      updatePinecone,
-      openApp,
-      takeScreenshot,
-      getVisualCrossingWeatherForecast,
-      huggingFaceImageClassification,
-    ],
-  });
+const functionChain = new FunctionChain({
+  functions: [
+    wikipedia,
+    fetchCryptoPrice,
+    getAlphaVantageCompanyOverview,
+    getAlphaVantageIntraday,
+    askPinecone,
+    createPinecone,
+    updatePinecone,
+    openApp,
+    takeScreenshot,
+    getVisualCrossingWeatherForecast,
+    huggingFaceImageClassification,
+  ],
+});
 
-  // 1. Wikipedia Example
-  let res1 = await functionChain.call(
-    "Look up on wikipedia what Langchain is in one sentence"
-  );
-  console.log("1. Wikipedia Example Result:", res1);
+const commands = [
+  "Look up on Wikipedia what Langchain is in one sentence.",
+  "What is the latest price of Bitcoin?",
+  "Get the current PE Ratio of Microsoft.",
+  "What was Apple's last intraday stock price?",
+  "Create a pinecone index called function-chain",
+  "Update my pinecone index called function-chain in the namespace hello-world with: This is a demonstration of upserting to Pinecone with natural language",
+  "What demonstrations are in my pinecone index called function-chain under the hello-world namespace?",
+  "Open the Calculator on my computer",
+  "Take a screenshot on my computer",
+  "Get the weather forecast for San Francisco Today",
+  "Classify this image: https://shorturl.at/ciTU6",
+];
 
-  // 2. Fetch Crypto Price Example
-  let res2 = await functionChain.call("Fetch the latest price of Bitcoin");
-  console.log("2. Fetch Crypto Price Example Result:", res2);
-
-  // 3. Alpha Vantage Example
-  let res3 = await functionChain.call(
-    "Get the overview of Microsoft",
-    getAlphaVantageCompanyOverview
-  );
-  console.log("3. Alpha Vantage Example Result:", res3);
-
-  let res4 = await functionChain.call(
-    "Get the intraday of Apple",
-    getAlphaVantageIntraday
-  );
-  console.log("4. Alpha Vantage Intraday Example Result:", res4);
-
-  // 5. Pinecone Example
-  let res5 = await functionChain.call(
-    "Create a pinecone index called function-chain",
-    createPinecone
-  );
-  console.log("5. Pinecone Example Result (Create):", res5);
-
-  let res6 = await functionChain.call(
-    "Update pinecone data with ID 1",
-    updatePinecone
-  );
-  console.log("6. Pinecone Example Result (Update):", res6);
-
-  let res7 = await functionChain.call(
-    "Ask Pinecone data with ID 1",
-    askPinecone
-  );
-  console.log("7. Pinecone Example Result (Ask):", res7);
-
-  // 8. Open App Example
-  let res8 = await functionChain.call("Open Slack", openApp);
-  console.log("8. Open App Example Result:", res8);
-
-  // 9. Take Screenshot Example
-  let res9 = await functionChain.call(
-    "Take a screenshot of the current window",
-    takeScreenshot
-  );
-  console.log("9. Take Screenshot Example Result:", res9);
-
-  // 10. Visual Crossing Weather Forecast Example
-  let res10 = await functionChain.call(
-    "Get the weather forecast for San Francisco",
-    getVisualCrossingWeatherForecast
-  );
-  console.log("10. Visual Crossing Weather Forecast Example Result:", res10);
-
-  // 11. Hugging Face Image Classification Example
-  let res11 = await functionChain.call(
-    "Classify an image of a cat",
-    huggingFaceImageClassification
-  );
-  console.log("11. Hugging Face Image Classification Example Result:", res11);
+for (const [commandIndex, command] of commands.entries()) {
+  const commandResult = await functionChain.call(command);
+  const logMessage = `${commandIndex + 1}. ${commandResult}`;
+  console.log(logMessage);
 }
-
-runAllExamples();
